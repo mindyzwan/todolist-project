7541d2be777eaf4621c35a33dbc72a621cbe1efa
mindyzwan <mindyzwan@gmail.com>
2019-06-30 19:30:35 +0000

    first commit

---
+++ todo.rb
@@ -0,0 +1,127 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View all of the lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Render the edit list name form
+get "/lists/:listid/edit" do
+  id = params[:listid].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# View specific list
+get "/lists/:listid" do
+  @list_id = params[:listid].to_i
+  @list = session[:lists][@list_id]
+  erb :individual_list
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# Edits a list
+post "/lists/:listid/edit" do
+  id = params[:listid].to_i
+  @list = session[:lists][id]
+
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
+
+# Deletes a list
+post "/lists/:listid/delete" do
+  id = params[:listid].to_i
+  session[:lists].delete_at(id)
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+  end
+
+# Adds todo items to a list
+post "/lists/:listid/todos" do
+  @list_id = params[:listid].to_i
+  @list = session[:lists][@list_id]
+
+  todo_name = params[:todo].strip
+  error = error_for_todo_name(todo_name)
+  if error
+    session[:error] = error
+    erb :individual_list
+  else
+    @list[:todos] << {name: todo_name, completed: false}
+    session[:success] = "The todo was aaded."
+    redirect "lists/#{@list_id}"
+  end
+end
+
+# Deletes todo items from a list
+post "/lists/:listid/todos/:todoid/delete" do
+  @list_id = params[:listid].to_i
+  @todos = session[:lists][@list_id][:todos]
+  todo_id = params[:todoid].to_i
+  @todos.delete_at(todo_id)
+
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
+
+helpers do
+  # Return an error message if the name is invalid. Return nil if name is valid.
+  def error_for_list_name(name)
+    if !(1..100).cover?(name.size)
+      "List name must be between 1 and 100 characters."
+    elsif session[:lists].any? { |list| list[:name] == name }
+      "List name must be unique."
+    end
+  end
+
+  # Return an error message if the name is invalid. Return nil if name is valid.
+  def error_for_todo_name(name)
+    if !(1..100).cover?(name.size)
+      "Todo must be between 1 and 100 characters."
+    end
+  end
+end
